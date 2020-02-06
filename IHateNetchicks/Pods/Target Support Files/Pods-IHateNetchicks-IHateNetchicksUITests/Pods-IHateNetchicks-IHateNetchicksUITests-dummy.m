4f64748e0db7949e263508270178ecb491dd7570
David Espiritu <despiritu@csumb.edu>
2020-02-05 22:40:54 -0800

    Initial commit

---
+++ IHateNetchicks/Pods/Target Support Files/Pods-IHateNetchicks-IHateNetchicksUITests/Pods-IHateNetchicks-IHateNetchicksUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IHateNetchicks_IHateNetchicksUITests : NSObject
+@end
+@implementation PodsDummy_Pods_IHateNetchicks_IHateNetchicksUITests
+@end
---
+++ IHateNetchicks/Pods/Target Support Files/Pods-IHateNetchicks-IHateNetchicksUITests/Pods-IHateNetchicks-IHateNetchicksUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_IHateNetchicks_IHateNetchicksUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_IHateNetchicks_IHateNetchicksUITestsVersionString[];
+
